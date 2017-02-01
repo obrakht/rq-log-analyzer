5bac25cb486bdd90ff6af055c62e545227ee782f
Dmitri <obrakht@gmail.com>
2017-02-01 23:22:37 +0300

    webpack settings

--- src/js/client.js
+++ src/js/client.js
@@ -5,11 +5,16 @@ class Main extends React.Component {
     render() {
         return (
           <div>
-            <h1>rqlog start</h1>
+            <h1>rqlog test 123</h1>
+            <form className="upload-form" action="" formMethod="post" formEncType="multipart/form-data">
+              <label className="upload-form__label">
+                <input className="upload-form__input" type="file" defaultValue="" name="log-file" />
+              </label>
+            </form>
           </div>
       );
     }
-}
+} 
 
 const app = document.getElementById('app');
 ReactDOM.render(<Main />, app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
-var path = require('path');
-var srcPath = path.join(__dirname, 'src');
-var buildPath = path.join(__dirname, 'dist');
+var path = require('path'),
+    srcPath = path.join(__dirname, 'src'),
+    buildPath = path.join(__dirname, 'dist');
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: srcPath,
-    entry: path.join(srcPath, 'js', 'client.js'),
+    entry: {
+        bundle: path.join(srcPath, 'js', 'client.js'),
+        styles: path.join(srcPath, 'scss', 'entry.scss')
+    },
     output: {
         path: buildPath,
-        filename: "bundle.js"
+        filename: "[name].js"
     },
+    plugins: [
+        new ExtractTextPlugin({ filename: 'styles.css', disable: false, allChunks: true })
+    ],
+    devtool: '#cheap-module-source-map',
     module: {
         loaders: [
             {
@@ -18,6 +26,24 @@ module.exports = {
                 query: {
                     presets: ['react', 'es2015']
                 }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style-loader', 
+                    loader: 'css-loader!sass-loader'
+                })
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style-loader', 
+                    loader: 'css-loader'
+                })
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
             }
         ]
     },
