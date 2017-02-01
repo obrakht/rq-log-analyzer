53389af221ac694f524c96dc16080cb9f3eebbf4
Dmitri <obrakht@gmail.com>
2017-02-01 21:55:12 +0300

    basic project setting

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var srcPath = path.join(__dirname, 'src');
+var buildPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    context: srcPath,
+    entry: path.join(srcPath, 'js', 'client.js'),
+    output: {
+        path: buildPath,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+    devServer: {
+        inline: true,
+        port: 8008
+    },
+};
